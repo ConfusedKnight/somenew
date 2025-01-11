ac6c5a3b9c30e287d60d2068cd50a05ea087774a
ConfusedKnight <aarushdangol2006@gmail.com>
2025-01-11 14:46:38 +0545

    updated the practice

--- 01.js
+++ 01.js
@@ -1,38 +1,85 @@
-let myname = "aarush";
-let age = 18;
-console.log(myname);
-console.log(age);
-const pi = 3.14; // you cannot change this variable's value
-//pi = 2;//gives error 
-console.log(pi);
+// let myname = "aarush";
+// let age = 18;
+// console.log(myname);
+// console.log(age);
+// const pi = 3.14; // you cannot change this variable's value
+// //pi = 2;//gives error 
+// console.log(pi);
 
-let a=10;
-let b = 3.14159;
-console.log(b.toFixed(2));
-console.log(b.toPrecision(2));
+// let a=10;
+// let b = 3.14159;
+// console.log(b.toFixed(2));
+// console.log(b.toPrecision(2));
 
-let naam = "Aarush";
-let ar_naam = Array.from(naam);
-console.log(naam);
-console.log(ar_naam);
-let p_name = ar_naam.toString();
-console.log(p_name);
-let randomstuff = "sdfsd";
-console.log("ba" + Number(randomstuff).toString().toLowerCase() + "as");
+// let naam = "Aarush";
+// let ar_naam = Array.from(naam);
+// console.log(naam);
+// console.log(ar_naam);
+// let p_name = ar_naam.toString();
+// console.log(p_name);
+// let randomstuff = "sdfsd";
+// console.log("ba" + Number(randomstuff).toString().toLowerCase() + "as");
 
-let price  = 1000000;
-console.log(price.toLocaleString("en-IN"));
+// let price  = 1000000;
+// console.log(price.toLocaleString("en-IN"));
 
-let price2 = "100000";
-console.log(Number(price2).toLocaleString("en"));
+// let price2 = "100000";
+// console.log(Number(price2).toLocaleString("en"));
 
-console.log(Math.ceil(3.14));
-console.log(Math.floor(3.14));
+// console.log(Math.ceil(3.14));
+// console.log(Math.floor(3.14));
 
-console.log(`this is a num ${price} and ${price = 2}`);
+// console.log(`this is a num ${price} and ${price = 2}`);
 
-let today = new Date();
-console.log(today);
-console.log(today.toString());
-console.log(today.toDateString());
-console.log(today.toLocaleDateString());
+// let today = new Date();
+// console.log(today);
+// console.log(today.toString());
+// console.log(today.toDateString());
+// console.log(today.toLocaleDateString());
+// console.log(today.getFullYear());
+// console.log(today.getMonth());
+// console.log(today.getHours());
+// console.log(today.getMinutes());
+// console.log(today.getSeconds());
+// let sit = "aarush";
+// for(i in sit){
+//     console.log(i);
+// }
+// let st = "drishitya";
+// for(i of st){
+//     console.log(st[i]);
+// }
+
+//fizzbuzz
+// let i;
+// for(i=1;i<=100;i++){
+//     if(i%3==0){
+//         if(i%5==0){
+//             console.log("fizzbuzz");
+//         }else{
+//             console.log("buzz");
+//         }
+//     }else if(i%5==0){
+//         console.log("fizz");
+//     }else{
+//         console.log(i);
+//     }
+// }
+
+// let arr = [1,2,3,4,5];
+// let arr2 = [...arr,6,7,8];
+// console.log(arr2);
+
+// const aardiv = (num)=> num/2;
+// console.log(aardiv(10))
+
+// let num = [1,2,3,4,5];
+
+// let task1 = num.map((x)=>x % 2 == 0);
+// console.log(task1);
+
+// let task2 = num.filter((x)=>x==5);
+// console.log(task2);
+
+// let task3 = num.reduce((acc=1,curr)=>acc*curr);
+// console.log(task3);
